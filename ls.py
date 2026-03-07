e727cdb6bc2afc84608b44f263bd98fab4a622f0
Sharath Kumar K <sharathkumar.k00@gmail.com>
2026-03-07 16:38:36 +0530

    Add parsing command line argument parsing
    Refactored a little
    Add 'argparse' module and defined some argument

--- ls.py
+++ ls.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 
-# pwd = os.getcwd()
-pwd = "/home/sharath/"
-items = []
+options = {
+    # Show hidden files (files starts with ".")
+    "all": False,
+    # Show indicators to directory, executable, links, pipes, sockets
+    # "classify": False,
+    # Show diffent classes in different colors
+    # "color": False,
+    # File to list
+    "file": False,
+}
 
 
-def main():
-    global pwd, items
+def option_parsing():
+    global options
+    opt_keys = options.keys()
+
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [options] [FILE]",
+        description="List directory contents."
+    )
+    parser.add_argument(
+        "-a", "--all", action="store_true",
+        help="do not ignore hiddle files (entries start with '.')"
+    )
+    parser.add_argument(
+        "file", nargs='?', default=None,
+        help="list information about FILEs."
+    )
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    for key in opt_keys:
+        options[key] = args_dict[key]
+
+
+def list_files(pwd):
+    items = []
     ls = os.listdir(pwd)
 
     for f in ls:
@@ -16,12 +47,22 @@ def main():
             print(pwd + f)
             return
         if os.path.isdir(pwd + f):
-            items.append(f + '/')
+            items.append(f + "/")
         else:
             items.append(f)
 
     items.sort()
-    print("\n".join(items))
+    return items
+
+
+def main():
+    # pwd = os.getcwd()
+    # pwd = "/home/sharath/"
+    # items = list_files(pwd)
+    # print("\n".join(items))
+    option_parsing()
+    print("Options provided:")
+    print(options)
 
 
 if __name__ == "__main__":
